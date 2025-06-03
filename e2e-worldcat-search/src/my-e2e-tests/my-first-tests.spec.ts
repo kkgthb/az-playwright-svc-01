efddca522be45988e4ad6845eae268f9e17dbc2a
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-06-03 18:15:21 -0500

    Fleshed out the Worldcat test

--- e2e-worldcat-search/src/my-e2e-tests/my-first-tests.spec.ts
+++ e2e-worldcat-search/src/my-e2e-tests/my-first-tests.spec.ts
@@ -1,7 +1,40 @@
-import { test, expect } from "@playwright/test";
-const url_to_visit = `https://search.worldcat.org/search?q=automation&itemSubType=book-printbook&limit=10&offset=21&itemSubTypeModified=book-printbook`;
+import { test, expect, type Page } from "@playwright/test";
+const url_to_visit = `https://search.worldcat.org/search?q=Katie&itemSubType=book-printbook&limit=10&offset=21&itemSubTypeModified=book-printbook`;
 
-test("Has correct phrase in title", async ({ page }) => {
-    await page.goto(url_to_visit);
-    await expect(page).toHaveTitle(/^automation - Search Results$/);
+test.describe.configure({ mode: 'serial' });
+let page: Page;
+
+test.describe('Worldcat search works', () => {
+
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+    })
+
+    test('Load the page, reject cookies, and validate the site title', async () => {
+        await page.goto(url_to_visit);
+        await page.getByRole('button', { name: 'Reject unnecessary cookies' }).click();
+        await expect(page).toHaveTitle(/^Katie - Search Results$/);
+    });
+
+    test('Validate that we are on page 3', async () => {
+        await expect(page.getByLabel(/^page 3$/)).toHaveText(`3`);
+    })
+
+    test('Validate that there are 10 search results (because we paginated at 10 and searched a common word)', async () => {
+        await expect(page.getByTestId(/^search-result-item-/)).toHaveCount(10);
+    })
+
+    test('Clear filter and validate that we are on page 1', async () => {
+        await page.getByTestId('facet-container-format-reset-button').click();
+        await expect(page.getByLabel(/^page 1$/)).toHaveText(`1`);
+    })
+
+    test('Validate that there are also 10 search results now', async () => {
+        await expect(page.getByTestId(/^search-result-item-/)).toHaveCount(10);
+    })
+
+    test.afterAll(async () => {
+        await page.close();
+    });
+
 });
