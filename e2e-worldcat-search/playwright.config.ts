883186eccf6ac78ea4a0c475436ed47c724ef3b7
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-06-04 08:32:54 -0500

    Style preference:  renamed local test output folder

--- e2e-worldcat-search/playwright.config.ts
+++ e2e-worldcat-search/playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: "./src/my-e2e-tests",
     testMatch: "**/*.@(spec|test|e2e)?(.playwright).?(c|m)[jt]s?(x)",
+    outputDir: "./.test-results",
     fullyParallel: true,
     projects: [
         {
