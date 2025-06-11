fc73730bc0bed3b3769fed6f084839281f6a1cb8
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-06-11 07:21:41 -0500

    Compare-and-contrast against BrowserStack Automate syntax

--- e2e-worldcat-search/playwright.config.ts
+++ e2e-worldcat-search/playwright.config.ts
@@ -20,6 +20,25 @@ export default defineConfig({
                 ...devices['iPhone 13'],
             },
         },
+        // // https://github.com/vikrantd1203/PW_Local/blob/f650bdc360d70d23de14cef5be2ef1cbb9217354/package.json#L9
+        // // https://www.browserstack.com/docs/automate/playwright/playwright-android#Using_legacy
+        // // https://www.browserstack.com/docs/automate/playwright/playwright-ios#Using_legacy
+        // // Allegedly, this stuff should work as an alternative to Microsoft Playwright Testing Service, 
+        // // but the docs don't match the official example, so keeping commented out for now.
+        // {
+        //     name: 'chrome@Samsung Galaxy S22:13@browserstack-mobile',
+        //     use: {
+        //         browserName: 'chromium',
+        //         channel: 'chrome'
+        //     }
+        // },
+        // {
+        //     name: 'safari@iPhone 15 Pro Max:17@browserstack-mobile',
+        //     use: {
+        //         browserName: 'safari',
+        //         channel: 'safari'
+        //     }
+        // }
     ],
     use: {
         trace: 'on',
